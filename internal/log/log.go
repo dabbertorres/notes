57f70bc1875d060a424f6ad5243c0e5d4717ce46
dabbertorres <adiverso93@gmail.com>
2024-07-01 09:18:33 -0700

    Fix incorrect caller in logs.

--- internal/log/log.go
+++ internal/log/log.go
@@ -9,29 +9,43 @@ import (
 )
 
 func Debug(ctx context.Context, msg string, fields ...zap.Field) {
-	scope.Logger(ctx).Debug(msg, fields...)
+	scope.Logger(ctx).
+		WithOptions(zap.AddCallerSkip(1)).
+		Debug(msg, fields...)
 }
 
 func Info(ctx context.Context, msg string, fields ...zap.Field) {
-	scope.Logger(ctx).Info(msg, fields...)
+	scope.Logger(ctx).
+		WithOptions(zap.AddCallerSkip(1)).
+		Info(msg, fields...)
 }
 
 func Warn(ctx context.Context, msg string, fields ...zap.Field) {
-	scope.Logger(ctx).Warn(msg, fields...)
+	scope.Logger(ctx).
+		WithOptions(zap.AddCallerSkip(1)).
+		Warn(msg, fields...)
 }
 
 func Error(ctx context.Context, msg string, fields ...zap.Field) {
-	scope.Logger(ctx).Error(msg, fields...)
+	scope.Logger(ctx).
+		WithOptions(zap.AddCallerSkip(1)).
+		Error(msg, fields...)
 }
 
 func DPanic(ctx context.Context, msg string, fields ...zap.Field) {
-	scope.Logger(ctx).DPanic(msg, fields...)
+	scope.Logger(ctx).
+		WithOptions(zap.AddCallerSkip(1)).
+		DPanic(msg, fields...)
 }
 
 func Panic(ctx context.Context, msg string, fields ...zap.Field) {
-	scope.Logger(ctx).Panic(msg, fields...)
+	scope.Logger(ctx).
+		WithOptions(zap.AddCallerSkip(1)).
+		Panic(msg, fields...)
 }
 
 func Fatal(ctx context.Context, msg string, fields ...zap.Field) {
-	scope.Logger(ctx).Fatal(msg, fields...)
+	scope.Logger(ctx).
+		WithOptions(zap.AddCallerSkip(1)).
+		Fatal(msg, fields...)
 }
